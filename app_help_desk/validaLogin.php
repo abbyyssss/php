6558442122357b72fccdbb1978b99bd20fe09118
Kauan Matheus <abbyysss66@gmail.com>
2025-06-11 13:44:43 -0300

    Help Desk V2

--- app_help_desk/validaLogin.php
+++ app_help_desk/validaLogin.php
@@ -1,59 +1,34 @@
-<?php 
+<?php
 session_start();
-
-
-$usuarios = array(
-['id' => '1',
-'perfil' => 'adm',
-'nome' => 'Kauan',
-'email' => 'kauanmcvq@gmail.com',
-'senha' => '123'
-], 
-['id' => '2',
-'perfil' => 'user',
-'nome' => 'Fernando',
-'email' => 'fefe@gmail.com',
-'senha' => '1234'
-], 
-['id' => '3',
-'perfil' => 'user',
-'nome' => 'pita',
-'email' => 'pitapica@gmail.com',
-'senha' => '12345'
-], 
-);
+require 'config.php';
 
 $usuarioAutenticado = false;
 
-// recebendo via get
+//RECEBENDO OS DADOS VIA MÉTODO GET
 $emailUsuario = $_GET['email'];
-$senhaUsuario = $_GET['senha'];
+$senhaUsuario = md5($_GET['senha']);
 
- //autenticando usuário
- for ($idx = 0; $idx < count ($usuarios); $idx++ ){
-    if ($emailUsuario == $usuarios[$idx]['email'] && $senhaUsuario == $usuarios [$idx]['senha'])  {
+//BUSCANDO NO BANCO AS INFORMAÇÕES
+    $sql = "SELECT * FROM usuarios WHERE email='{$emailUsuario}'";
+    $res = $conexao->query($sql);
+    $row = $res->fetch_object();
+
+// AUTENTICANDO O USUÁRIO
+    if ($emailUsuario == $row->email && $senhaUsuario == $row->senha) {
         $usuarioAutenticado = true;
-        $_SESSION['id'] = $usuarios[$idx]['id']; 
-        $_SESSION['perfil'] = $usuarios[$idx]['perfil']; 
-        $_SESSION['nome'] = $usuarios[$idx]['nome'];  
-        break;
-    }
-    else {
+        $_SESSION['id'] = $row-> id_usuario;
+        $_SESSION['perfil'] = $row->perfil;
+        $_SESSION['nome'] = $row->nome;
+    } else {
         $usuarioAutenticado = false;
     }
- }
-    
 
- if($usuarioAutenticado){
-    //validando a sessão
+if($usuarioAutenticado){
+    // VALIDANDO A SESSÃO
     $_SESSION['autenticado'] = 'sim';
     header('location: home.php');
-
- } else{
-    //validando a sessão
+} else {
+    // VALIDANDO A SESSÃO
     $_SESSION['autenticado'] = 'nao';
     header('location: index.php?login=erro');
- }
- 
- 
-?>
+}
